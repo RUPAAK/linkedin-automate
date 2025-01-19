7e4dfca98956e4b5e48be58416437c805b0a9335
rupaak <rupakt525@gmail.com>
2025-01-19 18:04:00 +0545

    deploy

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from crewai_tools import ScrapeWebsiteTool
-import os
-from agents import SocialMediaManagerAgent, ImageCreatorAgent
+# from crewai_tools import ScrapeWebsiteTool
+# import os
+# from agents import SocialMediaManagerAgent, ImageCreatorAgent
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-scrape_tool = ScrapeWebsiteTool(
-    website_url="https://www.freecodecamp.org/news/how-cdns-improve-performance-in-front-end-projects",
-)
-text = scrape_tool.run()
-
-
-with open("output.txt", "w") as file:
-    file.write(text)
-
-# Instantiate the agents
-data_analyst_agent = SocialMediaManagerAgent()
-# content_creator_agent = ImageCreatorAgent()
+# scrape_tool = ScrapeWebsiteTool(
+#     website_url="https://www.freecodecamp.org/news/how-cdns-improve-performance-in-front-end-projects",
+# )
+# text = scrape_tool.run()
 
 
-data_analyst_output = data_analyst_agent.kickoff()
-# content_creator_output = content_creator_agent.kickoff()
+# with open("output.txt", "w") as file:
+#     file.write(text)
+
+# # Instantiate the agents
+# data_analyst_agent = SocialMediaManagerAgent()
+# # content_creator_agent = ImageCreatorAgent()
+
+
+# data_analyst_output = data_analyst_agent.kickoff()
+# # content_creator_output = content_creator_agent.kickoff()
 
 print("Data Analyst Output:")
-print(data_analyst_output)
+# print(data_analyst_output)
